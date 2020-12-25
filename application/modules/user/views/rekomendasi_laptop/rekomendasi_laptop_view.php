e5d8f011968747e06acd6c6859a0aca7c3aff31c
Agung1234567890 <Agungstya88888@gmail.com>
2020-12-26 06:58:38 +0700

    x

--- application/modules/user/views/rekomendasi_laptop/rekomendasi_laptop_view.php
+++ application/modules/user/views/rekomendasi_laptop/rekomendasi_laptop_view.php
@@ -20,36 +20,92 @@
             <div class="card-header text-left">Masukkan Bobot</div>
             <div class="card-body">
                 <form action="" method="POST">
-                    <div class="form-group" style="margin-bottom:50px">
-                        <label for="merk">Merk <?= form_error('merk') ?></label>
+                    <!-- <div class="form-group" style="margin-bottom:50px">
+                        <label for="merk">Harga <?= form_error('harga') ?></label>
+                        <select class="form-control" name="harga" id="harga" style="width:100% !important">
+                            <option value="">- Pilih -</option>
+                            <option value="20" <?= $harga == 20 ? 'selected' : '' ?>>0 - 2.000.000</option>
+                            <option value="40" <?= $harga == 40 ? 'selected' : '' ?>>2.000.000 - 4.000.000</option>
+                            <option value="60" <?= $harga == 60 ? 'selected' : '' ?>>4.000.000 - 6.000.000</option>
+                            <option value="80" <?= $harga == 80 ? 'selected' : '' ?>>6.000.000 - 8.000.000</option>
+                            <option value="100" <?= $harga == 100 ? 'selected' : '' ?>>8.000.000 - 10.000.000</option>
+                            <option value="100" <?= $harga == 100 ? 'selected' : '' ?>>>10.000.000</option>
+                            <!-- <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>8.000.000 - 8.000.000</option>
+                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>9.000.000 - 2.000.000</option> -->
+                    <!-- </select>
+                    </div> -->
+                    <!-- <div class="form-group" style="margin-bottom:50px">
+                        <label for="merk">RAM <?= form_error('merk') ?></label>
                         <select class="form-control" name="merk" id="merk" style="width:100% !important">
                             <option value="">- Pilih -</option>
-                            <option value="1" <?= $merk == 1 ? 'selected' : '' ?>>HP</option>
-                            <option value="2" <?= $merk == 2 ? 'selected' : '' ?>>Macbook</option>
-                            <option value="3" <?= $merk == 3 ? 'selected' : '' ?>>Asus</option>
-                            <option value="4" <?= $merk == 4 ? 'selected' : '' ?>>Dell</option>
-                            <option value="5" <?= $merk == 5 ? 'selected' : '' ?>>MSI</option>
-                            <option value="6" <?= $merk == 6 ? 'selected' : '' ?>>Lenovo</option>
-                            <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>Acer</option>
-                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>Samsung</option>
-                        </select>
-                    </div>
-                    <!-- <div class="form-group">
+                            <option value="20" <?= $merk == 20 ? 'selected' : '' ?>>2 GB</option>
+                            <option value="40" <?= $merk == 40 ? 'selected' : '' ?>>4 GB</option>
+                            <option value="60" <?= $merk == 60 ? 'selected' : '' ?>>6 GB</option>
+                            <option value="80" <?= $merk == 80 ? 'selected' : '' ?>>8 GB</option>
+                            <option value="100" <?= $merk == 100 ? 'selected' : '' ?>>16 GB</option>
+                            <option value="100" <?= $merk == 100 ? 'selected' : '' ?>>>16 GB</option>
+                            <!-- <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>8.000.000 - 8.000.000</option>
+                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>9.000.000 - 2.000.000</option> -->
+                    <!-- </select>
+                    </div> --> --> -->
+                    <!-- <div class="form-group" style="margin-bottom:50px">
+                        <label for="merk">Penyimpanan Internal <?= form_error('merk') ?></label>
+                        <select class="form-control" name="merk" id="merk" style="width:100% !important">
+                            <option value="">- Pilih -</option>
+                            <option value="1" <?= $merk == 20 ? 'selected' : '' ?>>0 - 2.000.000</option>
+                            <option value="2" <?= $merk == 40 ? 'selected' : '' ?>>2.000.000 - 4.000.000</option>
+                            <option value="3" <?= $merk == 60 ? 'selected' : '' ?>>4.000.000 - 6.000.000</option>
+                            <option value="4" <?= $merk == 80 ? 'selected' : '' ?>>6.000.000 - 8.000.000</option>
+                            <option value="5" <?= $merk == 100 ? 'selected' : '' ?>>8.000.000 - 10.000.000</option>
+                            <option value="6" <?= $merk == 100 ? 'selected' : '' ?>>>10.000.000</option>
+                            <!-- <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>8.000.000 - 8.000.000</option>
+                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>9.000.000 - 2.000.000</option> -->
+                    <!-- </select>
+                    </div> --> -->
+                    <!-- <div class="form-group" style="margin-bottom:50px">
+                        <label for="merk">Harga <?= form_error('merk') ?></label>
+                        <select class="form-control" name="merk" id="merk" style="width:100% !important">
+                            <option value="">- Pilih -</option>
+                            <option value="1" <?= $merk == 20 ? 'selected' : '' ?>>0 - 2.000.000</option>
+                            <option value="2" <?= $merk == 40 ? 'selected' : '' ?>>2.000.000 - 4.000.000</option>
+                            <option value="3" <?= $merk == 60 ? 'selected' : '' ?>>4.000.000 - 6.000.000</option>
+                            <option value="4" <?= $merk == 80 ? 'selected' : '' ?>>6.000.000 - 8.000.000</option>
+                            <option value="5" <?= $merk == 100 ? 'selected' : '' ?>>8.000.000 - 10.000.000</option>
+                            <option value="6" <?= $merk == 100 ? 'selected' : '' ?>>>10.000.000</option>
+                            <!-- <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>8.000.000 - 8.000.000</option>
+                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>9.000.000 - 2.000.000</option> -->
+                    <!-- </select>
+                    </div> --> -->
+                    <!-- <div class="form-group" style="margin-bottom:50px">
+                        <label for="merk">Harga <?= form_error('merk') ?></label>
+                        <select class="form-control" name="merk" id="merk" style="width:100% !important">
+                            <option value="">- Pilih -</option>
+                            <option value="1" <?= $merk == 20 ? 'selected' : '' ?>>0 - 2.000.000</option>
+                            <option value="2" <?= $merk == 40 ? 'selected' : '' ?>>2.000.000 - 4.000.000</option>
+                            <option value="3" <?= $merk == 60 ? 'selected' : '' ?>>4.000.000 - 6.000.000</option>
+                            <option value="4" <?= $merk == 80 ? 'selected' : '' ?>>6.000.000 - 8.000.000</option>
+                            <option value="5" <?= $merk == 100 ? 'selected' : '' ?>>8.000.000 - 10.000.000</option>
+                            <option value="6" <?= $merk == 100 ? 'selected' : '' ?>>>10.000.000</option>
+                            <!-- <option value="7" <?= $merk == 7 ? 'selected' : '' ?>>8.000.000 - 8.000.000</option>
+                            <option value="8" <?= $merk == 8 ? 'selected' : '' ?>>9.000.000 - 2.000.000</option> -->
+                    <!-- </select>
+                    </div> --> -->
+                    <div class="form-group">
                         <label for="harga">Harga <br> (*presentase)</label>
-                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('harga') ?>" class="form-control" name="harga" id="harga" style="width:100% !important" placeholder="Masukkkan Nilai 0-100">
+                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('harga') ?>" class="form-control" name="harga" id="harga" style="width:100% !important">
                     </div>
                     <div class="form-group">
                         <label for="ram">Ram <br> (*presentase)</label>
-                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('ram') ?>" class="form-control" name="ram" id="ram" style="width:100% !important" placeholder="Masukkkan Nilai 0-100">
+                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('ram') ?>" class="form-control" name="ram" id="ram" style="width:100% !important">
                     </div>
                     <div class="form-group">
                         <label for="hdd">Penyimpanan internal<br> (*presentase)</label>
-                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('hdd') ?>" class="form-control" name="hdd" id="hdd" style="width:100% !important" placeholder="Masukkkan Nilai 0-100">
+                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('hdd') ?>" class="form-control" name="hdd" id="hdd" style="width:100% !important">
                     </div>
                     <div class="form-group">
                         <label for="processor">Processor <br> (*presentase)</label>
-                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('processor') ?>" class="form-control" name="processor" id="processor" style="width:100% !important" placeholder="Masukkkan Nilai 0-100">
-                    </div> -->
+                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('processor') ?>" class="form-control" name="processor" id="processor" style="width:100% !important">
+                    </div>
                     <!-- <div class="form-group">
                         <label for="resolusi">Resolusi</label>
                         <input type="text" onkeyup="isi()" value="<?= $this->input->post('resolusi') ?>" class="form-control" name="resolusi" id="resolusi" style="width:100% !important">
@@ -60,10 +116,10 @@
                     </div> -->
                     <div class="form-group">
                         <label for="vga">VGA Card <br> (*presentase)</label>
-                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('vga') ?>" class="form-control" name="vga" id="vga" style="width:100% !important" placeholder="Masukkkan Nilai 0-100">
+                        <input type="text" onkeyup="isi()" value="<?= $this->input->post('vga') ?>" class="form-control" name="vga" id="vga" style="width:100% !important">
                     </div>
-                    <span id="notif" style="color: red;font-size: 12px;">*Jumlah keseluruhan harus 100.</span><br>
-                    <button disabled type="submit" id="tombol" class="btn btn-sm btn-primary">Proses</button>
+                    <span id="notif" style="color: red;font-size: 12px;">*Jumlah Keseluruhan Harus = 100.</span><br>
+                    <button disabled type="submit" id="tombol" class="btn btn-sm btn-primary">PROSES</button>
                 </form>
                 <script>
                     isi();
