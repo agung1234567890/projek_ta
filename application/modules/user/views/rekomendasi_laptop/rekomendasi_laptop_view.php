7f0f9d3aeb87d57b53e7625cdb4ce5104c6b05b0
Agung1234567890 <Agungstya88888@gmail.com>
2020-12-26 07:40:48 +0700

    x

--- application/modules/user/views/rekomendasi_laptop/rekomendasi_laptop_view.php
+++ application/modules/user/views/rekomendasi_laptop/rekomendasi_laptop_view.php
@@ -6,7 +6,7 @@
 <div style="flex: 1;">
     <div class="alert alert-success" style="flex: 1;">
         <b>Masukkan Presentase Bobot Kepentingan Untuk Setiap Kriteria</b><br>
-        *Presentase harus diisi angka dari 0 sampai 100. Jumlah presentase bobot harus = 100
+        *Bobot harus diisi angka dari 0 sampai 100. Jumlah bobot harus = 100
     </div>
     <?php if ($this->session->flashdata('eror')) { ?>
         <div class="alert alert-danger">
